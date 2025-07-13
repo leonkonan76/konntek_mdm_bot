99309ea9b4bc097eb6e47acc0318fcbf14c0b49a
leonkonan76 <leonkonan@gmail.com>
2025-07-13 16:01:47 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
+# config.py
 import os
 
-# Lire le token depuis les variables d'environnement
+# Token du bot Telegram
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
+
+# IDs des administrateurs
 ADMIN_IDS = [int(x) for x in os.environ.get('ADMIN_IDS', '').split(',') if x]
 
-# Chemin du dossier data
-DATA_PATH = "./data"
-DB_NAME = "mdm_bot.db"
+# Chemins de données
+DATA_PATH = os.environ.get('DATA_PATH', './data')
+DB_NAME = os.environ.get('DB_NAME', 'mdm_bot.db')
 
-# Vérification (pour debug)
+# Validation
 if not BOT_TOKEN:
-    print("ATTENTION: BOT_TOKEN n'est pas configuré!")
+    raise ValueError("BOT_TOKEN n'est pas configuré")
+if not ADMIN_IDS:
+    print("Avertissement: Aucun ID administrateur configuré")
