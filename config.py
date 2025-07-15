7695e081fb90b2b020c2b2b62a34ce21be36d128
leonkonan76 <leonkonan@gmail.com>
2025-07-15 18:33:55 +0000

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ import os
 # Token du bot Telegram
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
 
+# Mot de passe pour accéder au bot
+BOT_PASSWORD = os.environ.get('BOT_PASSWORD', '')
+
 # IDs des administrateurs
 ADMIN_IDS = [int(x) for x in os.environ.get('ADMIN_IDS', '').split(',') if x]
 
@@ -14,5 +17,7 @@ DB_NAME = os.environ.get('DB_NAME', 'mdm_bot.db')
 # Validation
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN n'est pas configuré")
+if not BOT_PASSWORD:
+    raise ValueError("BOT_PASSWORD n'est pas configuré")
 if not ADMIN_IDS:
     print("Avertissement: Aucun ID administrateur configuré")
