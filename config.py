4ed58086f41755039ec62cc3224b4cb4a946e4c1
leonkonan76 <leonkonan@gmail.com>
2025-07-15 22:43:14 +0000

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-BOT_PASSWORD = "YOUR_SECURE_PASSWORD"
-ADMIN_IDS = "YOUR_ADMIN_IDS"  # Liste d'IDs séparés par des virgules
-DATA_PATH = "/opt/render/data"
-DB_NAME = "/opt/render/data/mdm_bot.db"
